858f4e0bdc4c91996a5ffc4fbac5e2cb7f6a2a6e
Randsw <randsw@mail.ru>
2025-10-01 17:53:12 +0300

    Working minio

--- setup-minio.sh
+++ setup-minio.sh
@@ -8,12 +8,14 @@ EOF
 
 # Deploy object store
   helm upgrade --install --wait --timeout 35m --atomic --namespace minio \
-    --repo https://operator.min.io minio-op tenant --values - <<EOF
+    --repo https://operator.min.io minio tenant --values - <<EOF
 tenant:
   # Pools (storage configuration)
   pools:
   - name: pool-0
     servers: 4
+    labels:
+      v1.min.io/tenant: myminio
     volumesPerServer: 4
     volumeClaimTemplate:
       metadata:
@@ -31,10 +33,110 @@ tenant:
     requestAutoCert: false
   buckets:
     - name: texts
-  prometheusOperator: true
+      objectLock: false
+    - name: results
+      objectLock: false
+  prometheusOperator: false
+  env:
+    - name: MINIO_PROMETHEUS_AUTH_TYPE
+      value: public
   serviceMetadata:
     minioServiceAnnotations:
       prometheus.io/scrape: "true"
       prometheus.io/path: "/minio/v2/metrics/cluster"
       prometheus.io/port: "9000"
+ingress:
+  api:
+    enabled: true
+    ingressClassName: nginx
+    host: minio.kind.cluster
+    path: /
+    pathType: Prefix
+  console:
+    enabled: true
+    ingressClassName: nginx
+    host: minio-console.kind.cluster
+    path: /
+    pathType: Prefix
+extraResources:
+  - |
+    apiVersion: operator.victoriametrics.com/v1beta1
+    kind: VMPodScrape
+    metadata:
+      name: minio-tenant
+      namespace: minio
+      labels:
+        app.kubernetes.io/name: minio-tenant
+    spec:
+      namespaceSelector:
+        matchNames:
+          - minio
+      selector:
+        matchLabels:
+          v1.min.io/tenant: myminio  # Adjust to your tenant name
+      podMetricsEndpoints:
+      - port: minio-port
+        path: /minio/v2/metrics/cluster
+        interval: 30s
+        scheme: http
+  - |
+    apiVersion: operator.victoriametrics.com/v1beta1
+    kind: VMPodScrape
+    metadata:
+      name: minio-resource
+      namespace: minio
+      labels:
+        app.kubernetes.io/name: minio-tenant
+    spec:
+      namespaceSelector:
+        matchNames:
+          - minio
+      selector:
+        matchLabels:
+          v1.min.io/tenant: myminio
+      podMetricsEndpoints:
+      - port: minio-port
+        path: /minio/v2/metrics/resource
+        interval: 30s
+        scheme: http
+  - |
+    apiVersion: operator.victoriametrics.com/v1beta1
+    kind: VMPodScrape
+    metadata:
+      name: minio-buckets
+      namespace: minio
+      labels:
+        app.kubernetes.io/name: minio-tenant
+    spec:
+      namespaceSelector:
+        matchNames:
+          - minio
+      selector:
+        matchLabels:
+          v1.min.io/tenant: myminio
+      podMetricsEndpoints:
+      - port: minio-port
+        path: /minio/v2/metrics/bucket
+        interval: 30s
+        scheme: http
+  - |
+    apiVersion: operator.victoriametrics.com/v1beta1
+    kind: VMPodScrape
+    metadata:
+      name: minio-nodes
+      namespace: minio
+      labels:
+        app.kubernetes.io/name: minio-tenant
+    spec:
+      namespaceSelector:
+        matchNames:
+          - minio
+      selector:
+        matchLabels:
+          v1.min.io/tenant: myminio
+      podMetricsEndpoints:
+      - port: minio-port
+        path: /minio/v2/metrics/node
+        interval: 30s
+        scheme: http
 EOF
