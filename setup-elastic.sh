277b44fe77965d0cc7e071345c7cd8b649613bf6
Randsw <randsw@mail.ru>
2025-10-05 15:47:52 +0300

    Add monitoring to elasticsearch

--- setup-elastic.sh
+++ setup-elastic.sh
@@ -15,9 +15,59 @@ podMonitor:
 EOF
 
   helm upgrade --install --wait --timeout 35m --atomic --namespace elastic --create-namespace \
-    --repo https://helm.elastic.co eck-stack eck-stack --values - <<EOF
+    --repo https://helm.elastic.co eck-stack eck-stack --values - <<'EOF'
 eck-elasticsearch:
   enabled: true
+  nodeSets:
+  - name: default
+    count: 1
+    config:
+      node.store.allow_mmap: false
+      xpack.security.authc:
+        anonymous:
+          username: anonymous_user
+          roles: remote_monitoring_collector
+          authz_exception: true
+    podTemplate:
+      spec:
+        containers:
+        - name: elasticsearch-exporter
+          image: quay.io/prometheuscommunity/elasticsearch-exporter:v1.9.0
+          ports:
+          - containerPort: 9114
+            name: metrics
+          env:
+          - name: ES_URI
+            value: "https://elasticsearch-es-http:9200"
+          - name: ES_PASSWORD
+            valueFrom:
+              secretKeyRef:
+                name: elasticsearch-es-elastic-user
+                key: elastic
+          - name: ES_USERNAME
+            value: "elastic"
+          args:
+          - --es.uri=$(ES_URI)
+          - --es.all
+          - --web.listen-address=:9114
+          - --web.telemetry-path=/metrics
+          - --es.ssl-skip-verify
+          resources:
+            requests:
+              memory: "64Mi"
+              cpu: "50m"
+            limits:
+              memory: "128Mi"
+              cpu: "100m"
+          securityContext:
+            runAsUser: 1000  # Match Elasticsearch user ID
+            runAsGroup: 1000
+            allowPrivilegeEscalation: false
+  monitoring:
+    metrics:
+      elasticsearchRefs:
+        - name: elasticsearch
+          namespace: elastic
   ingress:
     enabled: true
     className: nginx
@@ -29,6 +79,43 @@ eck-elasticsearch:
         path: /
 eck-kibana:
   enabled: true
+  config:
+    monitoring.kibana.collection.enabled: true
+    monitoring.ui.enabled: true
+    status.allowAnonymous: true
+  podTemplate:
+    spec:
+      containers:
+        - name: kibana-prometheus-exporter
+          image: chamilad/kibana-prometheus-exporter:v8.7.x.2
+          args:
+          - -kibana.uri=https://localhost:5601
+          - -kibana.skip-tls=true
+          - -wait=true
+          securityContext:
+            privileged: false
+            allowPrivilegeEscalation: false
+          resources:
+            limits:
+              memory: 100Mi
+              cpu: 100m
+            requests:
+              cpu: 10m
+              memory: 50Mi
+          ports:
+            - containerPort: 9684
+              name: metrics
+          livenessProbe:
+            httpGet:
+              path: /healthz
+              port: 9684
+            initialDelaySeconds: 10
+            periodSeconds: 10
+  monitoring:
+    metrics:
+      elasticsearchRefs:
+        - name: elasticsearch
+          namespace: elastic
   ingress:
     enabled: true
     className: nginx
@@ -37,4 +124,40 @@ eck-kibana:
       nginx.ingress.kubernetes.io/backend-protocol: "HTTPS"
     hosts:
       - host: kibana.kind.cluster
+EOF
+
+cat << EOF | kubectl apply -f -
+apiVersion: monitoring.coreos.com/v1
+kind: PodMonitor
+metadata:
+  name: elasticsearch-metrics
+  namespace: elastic
+spec:
+  selector:
+    matchLabels:
+      elasticsearch.k8s.elastic.co/cluster-name: elasticsearch # Directly select ES pods
+  podMetricsEndpoints:
+  - port: metrics # The name of the port exposed by the ES pod (ECK sets this automatically)
+    path: /metrics
+    interval: 30s
+  namespaceSelector:
+    matchNames:
+    - elastic
+---
+apiVersion: monitoring.coreos.com/v1
+kind: PodMonitor
+metadata:
+  name: kibana-metrics
+  namespace: elastic
+spec:
+  selector:
+    matchLabels:
+      kibana.k8s.elastic.co/name: eck-stack-eck-kibana
+  podMetricsEndpoints:
+  - port: metrics
+    path: /metrics
+    interval: 30s
+  namespaceSelector:
+    matchNames:
+    - elastic
 EOF
